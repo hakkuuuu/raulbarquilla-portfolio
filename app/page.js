72bd2b50dece7dcbb06a29eb0b4dfbab44eca7ca
Raul <raulbarquilla003@gmail.com>
2025-01-29 12:20:29 +0800

    added framer motion

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 'use client';
+import { motion } from 'framer-motion';
 import About from './components/About';
 import Contact from './components/Contact';
 import Hero from './components/Hero';
@@ -9,10 +10,42 @@ export default function Home() {
   return (
     <>
       <NavBar />
-      <Hero />
-      <About />
-      <Projects />
-      <Contact />
+
+      <motion.div
+        id="hero"
+        initial={{ opacity: 0, scale: 0.9 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 1, ease: "easeOut" }}
+      >
+        <Hero />
+      </motion.div>
+
+      <motion.div
+        id="about"
+        initial={{ opacity: 0, x: -50 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        transition={{ duration: 1, ease: "easeOut" }}
+      >
+        <About />
+      </motion.div>
+
+      <motion.div
+        id="projects"
+        initial={{ opacity: 0, y: 50, scale: 0.95 }}
+        whileInView={{ opacity: 1, y: 0, scale: 1 }}
+        transition={{ duration: 1, ease: "easeInOut" }}
+      >
+        <Projects />
+      </motion.div>
+
+      <motion.div
+        id="contact"
+        initial={{ opacity: 0, y: 50, scale: 0.95 }}
+        whileInView={{ opacity: 1, y: 0, scale: 1 }}
+        transition={{ duration: 1, ease: "easeInOut" }}
+      >
+        <Contact />
+      </motion.div>
     </>
   );
 }
